58723007d733110e4f9ece3fcc82b96587e56000
Stephan Houwaart <stephanhouwaart@hotmail.com>
2020-07-03 01:31:47 +0200

    finished styling the waitinscreen page

--- client/src/extern/Waitingscreen.js
+++ client/src/extern/Waitingscreen.js
@@ -8,7 +8,7 @@ class Waitingscreen extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      speed: 1,
+      speed: 1.5,
     }
   }
 
@@ -16,14 +16,17 @@ class Waitingscreen extends React.Component {
 
   render(){
 
-
-
     return(
       <article className="waitingscreen">
         <section className="waitingscreen__imageholder">
           <img style={{animation: `leftBear ${this.state.speed}s linear infinite alternate`}} className="waitingscreen__imageholder__image" src="/img/winner.png" alt="Golden bear"/>
-          <img className="waitingscreen__imageholder__image" src="/img/runnerup.png" alt="Silver bear"/>
-          <img className="waitingscreen__imageholder__image" src="/img/third.png" alt="Bronze bear"/>
+          <img style={{animation: `middleBear ${this.state.speed}s linear infinite alternate`}} className="waitingscreen__imageholder__image" src="/img/runnerup.png" alt="Silver bear"/>
+          <img style={{animation: `rightBear ${this.state.speed}s linear infinite alternate`}} className="waitingscreen__imageholder__image" src="/img/third.png" alt="Bronze bear"/>
+        </section>
+
+        <section className="waitingscreen__textholder">
+          <h2>The Host is checking your answers!</h2>
+          <h3>Please be patient</h3>
         </section>
       </article>
     );
