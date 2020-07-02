be3f9d4d1ce13d73131b4bb45197d3878d40ed4c
Stephan Houwaart <stephanhouwaart@hotmail.com>
2020-07-02 23:27:27 +0200

    added table to model

--- app/Score.php
+++ app/Score.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Score extends Model
 {
+    protected $table = 'score';
     protected $fillable = ['team', 'score'];
+    public $timestamps = false;
 }
